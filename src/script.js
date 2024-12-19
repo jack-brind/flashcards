ab88dfab7597b5d4b22b498fa45dc5fea44b0127
Jack Brind <63796862+brindo91@users.noreply.github.com>
2024-12-19 16:09:14 +0000

    Possible image fix

--- src/script.js
+++ src/script.js
@@ -5,42 +5,42 @@ const javascriptTips = [
     title: 'Nullish Coalescing Operator',
     summary:
       "Nullish Coalescing Operator works with nullish values, not falsy values. This will evaluate null or undefined as falsy but treat 0 or '' as truthy. Therefore, zero or empty strings are accepted.",
-    snippet: './public/snippets/nullish.png',
+    snippet: '/snippets/nullish.png',
     highlight: "It's good for working with default values.",
   },
   {
     title: 'Math.trunc()',
     summary:
       'This method returns the integer part of a number by removing any demical points.',
-    snippet: './public/snippets/trunc.png',
+    snippet: '/snippets/trunc.png',
   },
   {
     title: 'Math rounding',
     summary: 'To round a value, use floor, ceil and round.',
-    snippet: './public/snippets/math functions.png',
+    snippet: '/snippets/math functions.png',
     highlight: "There's a lot more that can be done here",
   },
   {
     title: 'Or operator',
     summary: 'Use || to or stuff',
-    snippet: './public/snippets/nulllish-operator1.png',
+    snippet: '/snippets/nullish.png',
     highlight: 'This is some random highlight3',
   },
   {
     title: 'Parameters',
     summary: 'Used for passing stuff to functions',
-    snippet: './public/snippets/nulllish-operator1.png',
+    snippet: '/snippets/nulllish-operator1.png',
     highlight: 'This is some random highlight2',
   },
   {
     title: 'For of loop',
     summary: 'Used to loop arrays',
-    snippet: './public/snippets/nulllish-operator1.png',
+    snippet: '/snippets/nulllish-operator1.png',
   },
   {
     title: 'Getting a DOM reference',
     summary: 'This is the summary',
-    snippet: './public/snippets/docbyid.png',
+    snippet: '/snippets/docbyid.png',
     highlight: 'This is some random highlight',
   },
 ];
